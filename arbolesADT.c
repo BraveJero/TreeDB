b82f655e7f0e05960fab0043b5505cf67a72990b
Jero Brave <jbrave271@gmail.com>
2020-07-06 13:19:03 -0300

    Esqueleto del main.

---
+++ arbolesADT.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+//Espacio para includes.
+//
+//
+//
+//
+//
+//
+
+//Espacio para definir defines.
+//
+//
+//
+//
+//
+//
+
+
+
+
+int main(int argc, char const *argv[]){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
