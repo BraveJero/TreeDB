daaa48c23d264cd931bdffb27a7b2aea00f4c50e
festevezz <festevez@itba.edu.ar>
2020-07-07 17:34:27 -0300

    Avances en el desarrollo del TAD. Se decidio utilizar vectores en vez de listas para el manejo de los datos.

--- barriosADT.c
+++ barriosADT.c
@@ -4,7 +4,7 @@
 #include "barriosADT.h"
 
 typedef struct tBarrio{
-  char * nombre;
+  char * nombre; // Nombre del barrio
   size_t cantArb; // Cantidad de arboles en el barrio
   size_t hab; // Cantidad de habitantes del barrio
 }tBarrio;
@@ -12,12 +12,17 @@ typedef struct tBarrio{
 // Nuestro CDT es un vector de tBarrio
 typedef struct barriosCDT{
   tBarrio * barrios;
-  size_t current;
+  size_t current; // Iterador actual
   size_t max; // Cantidad de barrios almacenados
 }barriosCDT;
 
 barriosADT newBarrio(){
-  return calloc(1, sizeof(barriosCDT));
+  barriosADT barrio = calloc(1, sizeof(barriosCDT));
+  if (barrio == NULL){
+    fprintf(stderr, "Espacio de memoria insuficiente.\n");
+    exit(1);
+  }
+  return barrio;
 }
 
 void freeBarrio(barriosADT ciudad){
@@ -25,24 +30,54 @@ void freeBarrio(barriosADT ciudad){
     free(ciudad->barrios[i].nombre);
   free(ciudad->barrios);
   free(ciudad);
+  return;
 }
 
 void addBarrio(barriosADT ciudad, const char * barrio, size_t hab){
   size_t pos = ciudad->max++;
-  ciudad->barrios = realloc(ciudad->barrios, (ciudad->max) * sizeof(tBarrio)); //Chequear que no retorne NULL
+  ciudad->barrios = realloc(ciudad->barrios, (ciudad->max) * sizeof(tBarrio));
+  if (ciudad->barrios == NULL){
+    fprintf(stderr, "Espacio de memoria insuficiente.\n");
+    exit(1);
+  }
   ciudad->barrios[pos].nombre = malloc(sizeof(char) * (strlen(barrio) + 1));
+  if (ciudad->barrios[pos].nombre == NULL){
+    fprintf(stderr, "Espacio de memoria insuficiente.\n");
+    exit(1);
+  }
+  strcpy(ciudad->barrios[pos].nombre, barrio);
   ciudad->barrios[pos].cantArb = 0;
   ciudad->barrios[pos].hab = hab;
+  return;
 }
 
-void addArbol(barriosADT ciudad, const char * barrio);
+void addArbol(barriosADT ciudad, const char * barrio){
+  for (size_t i = 0; i < ciudad->max; i++)
+    if (!strcmp(ciudad->barrios[i].nombre, barrio))
+      ciudad->barrios[i].cantArb++;
+  return;
+}
 
-void ordenCant(barriosADT ciudad);
+void ordenCant(barriosADT ciudad); // TO-DO
 
-void ordenDens(barriosADT ciudad);
+void ordenDens(barriosADT ciudad); // TO-DO
 
-void toBegin(barriosADT ciudad);
+void toBegin(barriosADT ciudad){
+  ciudad->current = 0;
+  return;
+}
 
-int hasNext(barriosADT ciudad);
+int hasNext(barriosADT ciudad){
+  return (ciudad->current < ciudad->max);
+}
 
-void next(barriosADT ciudad, char ** nombre, size_t * cantArb, size_t * hab);
+void next(barriosADT ciudad, char ** nombre, size_t * cantArb, size_t * hab){
+  if (!hasNext(ciudad))
+    return;
+  *nombre = ciudad->barrios[ciudad->current].nombre;
+  *cantArb = ciudad->barrios[ciudad->current].cantArb;
+  if (hab != NULL)
+    *hab = ciudad->barrios[ciudad->current].hab;
+  ciudad->current++;
+  return;
+}
