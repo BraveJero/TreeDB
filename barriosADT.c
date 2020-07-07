0790e7d9b357c10cd98e59bfb88f8a5d6dcc3884
juanigarcia <juanigarcia@itba.edu.ar>
2020-07-07 16:15:17 -0300

    Desarrollo de las funcionalidades de los TADs

--- barriosADT.c
+++ barriosADT.c
@@ -7,10 +7,42 @@ typedef struct tBarrio{
   char * nombre;
   size_t cantArb; // Cantidad de arboles en el barrio
   size_t hab; // Cantidad de habitantes del barrio
-  struct tBarrio * tail;
 }tBarrio;
 
 // Nuestro CDT es un vector de tBarrio
 typedef struct barriosCDT{
-  tBarrio * first; // Puntero al primero barrio
+  tBarrio * barrios;
+  size_t current;
+  size_t max; // Cantidad de barrios almacenados
 }barriosCDT;
+
+barriosADT newBarrio(){
+  return calloc(1, sizeof(barriosCDT));
+}
+
+void freeBarrio(barriosADT ciudad){
+  for(size_t i = 0; i < ciudad->max; i++)
+    free(ciudad->barrios[i].nombre);
+  free(ciudad->barrios);
+  free(ciudad);
+}
+
+void addBarrio(barriosADT ciudad, const char * barrio, size_t hab){
+  size_t pos = ciudad->max++;
+  ciudad->barrios = realloc(ciudad->barrios, (ciudad->max) * sizeof(tBarrio)); //Chequear que no retorne NULL
+  ciudad->barrios[pos].nombre = malloc(sizeof(char) * (strlen(barrio) + 1));
+  ciudad->barrios[pos].cantArb = 0;
+  ciudad->barrios[pos].hab = hab;
+}
+
+void addArbol(barriosADT ciudad, const char * barrio);
+
+void ordenCant(barriosADT ciudad);
+
+void ordenDens(barriosADT ciudad);
+
+void toBegin(barriosADT ciudad);
+
+int hasNext(barriosADT ciudad);
+
+void next(barriosADT ciudad, char ** nombre, size_t * cantArb, size_t * hab);
--- barriosADT.h
+++ barriosADT.h
@@ -4,7 +4,37 @@
 #ifndef BARRIOS_H
 #define BARRIOS_H
 
-typedef barrioCDT * barrioADT;
+typedef struct barriosCDT * barriosADT;
 
+// Crea un CDT vacio, es decir sin ningun barrio cargado
+barriosADT newBarrio();
+
+// Libera los recursos reservados por el TAD
+void freeBarrio(barriosADT ciudad);
+
+/* Agrega un barrio con su cantidad de habitantes
+** utilizaremos esta funcion para cargar la informacion de
+** los archivos barriosBUE.csv y barriosVAN.csv */
+void addBarrio(barriosADT ciudad, const char * barrio, size_t hab);
+
+/* Agrega un arbol al barrio
+** utilizaremos esta funcion para cargar la informacion de
+** los archivos arbolesBUE.csv y arbolesVAN.csv */
+void addArbol(barriosADT ciudad, const char * barrio);
+
+// Ordena los barrios de mayor a menor por cantidad de arboles
+void ordenCant(barriosADT ciudad);
+
+// Ordena los barrios de mayor a menor por densidad de arboles
+void ordenDens(barriosADT ciudad);
+
+// Setea el current en el primer barrio
+void toBegin(barriosADT ciudad);
+
+// Indica si hay un siguiente barrio o no
+int hasNext(barriosADT ciudad);
+
+// Devuelve los datos del barrio actual
+void next(barriosADT ciudad, char ** nombre, size_t * cantArb, size_t * hab);
 
 #endif
